4fa634e414f4861e6f7237bdd48c83ea13c97415
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-02-13 16:29:42 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     
 
     public String play(int number) {
+        
+        
 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
